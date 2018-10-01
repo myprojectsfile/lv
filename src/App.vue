ecbe281bb288b7edeac2e01271bc39643f2c15f4
Ahmadi.Mohammad <ahmadi80@gmail.com>
2018-10-01 15:29:46 +0330

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="message" @msgChanged="message=$event" :myFunc="logto"/>
+    <button class="btn btn-primary btn-sm" @click="changeMsg">change message</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: () => {
+    return {
+      message: "سلام به دوستان گلم"
+    };
+  },
+  methods: {
+    changeMsg() {
+      this.message = "سلام به همه شما عزیزان";
+    },
+    logto() {
+      alert('this is me');
+    }
+  },
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "calibri", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
